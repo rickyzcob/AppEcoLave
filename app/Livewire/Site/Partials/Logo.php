9c3cc780637e82bb50f57a700c51f39eea5dc02e
Ricardo Lima <ricardo@criareinfo.com.br>
2026-08-04 11:24:45 -0300

    ajustes na logo do site

--- app/Livewire/Site/Partials/Logo.php
+++ app/Livewire/Site/Partials/Logo.php
@@ -2,10 +2,21 @@
 
 namespace App\Livewire\Site\Partials;
 
+use App\Repositories\Admin\Configurations\ConfigurationsRepository;
 use Livewire\Component;
 
 class Logo extends Component
 {
+    public $configuration;
+    public function mount()
+    {
+        $configurationsRepository = new ConfigurationsRepository();
+        $configurationReturnDB =  $configurationsRepository->show()['data'];
+
+        if($configurationReturnDB != null){
+              $this->configuration = $configurationReturnDB;
+        }
+    }
     public function render()
     {
         return view('livewire.site.partials.logo');
--- resources/views/livewire/site/partials/logo.blade.php
+++ resources/views/livewire/site/partials/logo.blade.php
@@ -1,3 +1,9 @@
 <div>
-    <a class="logo" href="{{ route('home') }}">Eco<span>Lave</span></a>
+    @if(!$configuration)
+        <a class="logo" href="{{ route('home') }}">Seu<span> Site</span></a>
+    @elseif($configuration['logo'] === null)
+        <a class="logo" href="{{ route('home') }}"> {{$configuration['name']}}</a>
+    @elseif($configuration['logo'] !== null)
+        <a class="logo" href="{{ route('home') }}"> <img src="{{asset('storage/'.$configuration['logo'])}} " alt="Logotipo" class="h-20"></a>
+    @endif
 </div>
